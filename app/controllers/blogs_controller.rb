6bbb47e49ee50ff93ec7888a419b8605f2469fcd
keisuketomita <keisuke.tommy@gmail.com>
2020-10-20 10:59:22 +0000

    投稿編集・削除・バリデーション

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -26,7 +26,7 @@ class BlogsController < ApplicationController
   end
   def update
     if @blog.update(blog_params)
-      redirect_to blogs_path, "ブログを編集しました"
+      redirect_to blogs_path, notice: "ブログを編集しました"
     else
       render :edit
     end
